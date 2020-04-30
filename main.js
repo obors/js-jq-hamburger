f8f9afd0d509ddc24c07cac902a23b827db648da
Oleg <obors@ymail.com>
2020-04-30 03:38:39 +0200

    cileca sbaglio qualcosa

---
+++ main.js
@@ -0,0 +1,31 @@
+// $(".hamburger-menu.active").click(function (){
+//   ('.hamburger-menu').css('display','block');
+//
+// });
+//
+//
+// function showHide(){
+//   var click = document.getElementByClassName("fas fa-times")
+//   if (("hamburger-menu").style.display="none"){
+//     ("hamburger-menu").style.display="block";
+//
+//   }else {
+//     ("hamburger-menu").style.display="none";
+//   }
+//
+// }
+
+//   document.getElementsByClassName("close").addEventListener("click", myFunction);
+  // var hamburgerMenu = getElementsByClassName("hamburger-menu");
+//   function myFunction() {
+//   document.getElementById("LinkDisplay").style.display="block" ;
+// }
+// $("i").click(function ()){
+//
+//   $(".hamburger-menu").addClass("visible")
+// })
+// .click(function() {
+//     // il mouse è uscito dal cerchio
+//     // nascondere la scritta => togliere la classe 'visibile'
+//     $('.cerchio h1').removeClass('visible');
+// })
